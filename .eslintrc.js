1cdc8ddd82c8c9ee2e2732ba1f78c7135f62b065
paullee714 <ba93love@gmail.com>
2020-08-04 11:29:17 +0900

    vue init, eslint,prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
         singleQuote: true,
         semi: true,
         useTabs: false,
-        tabWidth: 2,
+        tabWidth: 4,
         trailingComma: "all",
         printWidth: 80,
         bracketSpacing: true,
