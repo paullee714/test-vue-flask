05d8ecbc8183867abe38905dde87cf4e538cc7b2
paullee714 <ba93love@gmail.com>
2020-08-04 11:36:47 +0900

    vue configure source reference

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
@@ -10,13 +10,13 @@
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-  created() {
-    // let a = 10; // veu.config.js로 에러를 back에서 확인
-    // console.log("hi") // eslintrc.js 에서 console.log 환경설
-  },
+    name: 'Home',
+    components: {
+        HelloWorld,
+    },
+    created() {
+        // let a = 10; // veu.config.js로 에러를 back에서 확인
+        // console.log("hi") // eslintrc.js 에서 console.log 환경설
+    },
 };
 </script>
